bd36f6e834251127ac47897921022bb590ca2100
Piotr Majchrowski <Piotr.Majchrowski@cdsazure.onmicrosoft.com>
2023-09-05 14:32:17 +0200

    playing with vue js

--- OnlineShop.UI/Pages/Admin/main.js
+++
@@ -1,6 +0,0 @@
-﻿var app = new Vue({
-    el: '#app',
-    data: {
-        price: 0
-    }
-})
---
+++ OnlineShop.UI/wwwroot/js/admin/main.js
@@ -0,0 +1,18 @@
+﻿const { createApp, ref, computed } = Vue
+
+createApp({
+    setup() {
+        const message = ref('Admin Panel')
+        const price = ref(0)
+
+        const formatPrice = computed(() => {
+            return "$" + price.value;
+        })
+
+        return {
+            message,
+            price,
+            formatPrice
+        }
+    }
+}).mount('#app')
